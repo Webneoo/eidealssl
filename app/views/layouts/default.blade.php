2dd7c7017e7cf264f4247c130eec96bd7c352df0
Nour Fakhoury <info@webneoo.com>
2017-12-20 12:16:58 +0200

    optimizing w3c part 1

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -32,7 +32,6 @@
 
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
 
-
     <!-- Facebook Pixel Code -->
     <script>
     !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
@@ -43,13 +42,11 @@
     fbq('init', '1509924475747974'); // Insert your pixel ID here.
     fbq('track', 'PageView');
     </script>
-    <noscript><img height="1" width="1" style="display:none"
-    src="https://www.facebook.com/tr?id=1509924475747974&ev=PageView&noscript=1"
-    /></noscript>
+    <noscript>
+        <img height="1" width="1" style="display:none" src="https://www.facebook.com/?id=1509924475747974&ev=PageView&noscript=1"/>
+    </noscript>
     <!-- DO NOT MODIFY -->
     <!-- End Facebook Pixel Code -->
-
-
     </head>
 
 <body>
@@ -81,22 +78,20 @@
 
     <script>
 
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 
-    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
 
-    ga('create', 'UA-76429814-1', 'auto');
+        ga('create', 'UA-76429814-1', 'auto');
 
-    ga('send', 'pageview');
+        ga('send', 'pageview');
 
-   
-
-  </script>
+    </script>
 
 
 </body>
