01f25995fbf3aec4ca5dda0e783f7dac03006aa4
Nour Fakhoury <info@webneoo.com>
2018-01-08 17:19:16 +0200

    .env

--- .env.php
+++ .env.php
@@ -1,25 +1,25 @@
 <?php
 
 // online
-// return array(
-//     'DB_HOST' => 'localhost',
-//     'DB_NAME' => 'eideal',
-//     'DB_USERNAME' => 'michel',
-//     'DB_PASSWORD' => '1be783ec',
-//     'MANDRILL_SECRET' => 'f5sMDJOqFLsIxE0nURi3Iw'
-// );
+return array(
+    'DB_HOST' => 'localhost',
+    'DB_NAME' => 'eideal',
+    'DB_USERNAME' => 'michel',
+    'DB_PASSWORD' => '1be783ec',
+    'MANDRILL_SECRET' => 'f5sMDJOqFLsIxE0nURi3Iw'
+);
 
 
 // localhost
 
-return array(
+// return array(
 
-    'DB_HOST' => 'localhost',
-    'DB_NAME' => 'eideal',
-    'DB_USERNAME' => 'homestead',
-    'DB_PASSWORD' => 'secret',
-    'MANDRILL_SECRET' => 'f5sMDJOqFLsIxE0nURi3Iw'
-);
+//     'DB_HOST' => 'localhost',
+//     'DB_NAME' => 'eideal',
+//     'DB_USERNAME' => 'homestead',
+//     'DB_PASSWORD' => 'secret',
+//     'MANDRILL_SECRET' => 'f5sMDJOqFLsIxE0nURi3Iw'
+// );
 
 
 ?>
