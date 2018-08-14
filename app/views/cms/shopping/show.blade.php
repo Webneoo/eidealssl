4aa4b7fcbe33df587d9732b0055446af5c9747c8
Nour Fakhoury <info@webneoo.com>
2018-08-14 17:18:51 +0300

    format number purchase amount CMS

--- app/views/cms/shopping/show.blade.php
+++ app/views/cms/shopping/show.blade.php
@@ -45,11 +45,11 @@
                     <td>{{ $t->purchase_date }}</td>
                     <td>
                         @if($t->order_status_id == 2 || $t->order_status_id == 6)
-                             <b style="color:orange">
+                             <b style="color:orange; font-size:13px;">
                         @elseif($t->order_status_id == 3 || $t->order_status_id == 5 || $t->order_status_id == 8)
-                             <b style="color:green">
+                             <b style="color:green; font-size:13px;">
                         @elseif($t->order_status_id == 4 || $t->order_status_id == 7 || $t->order_status_id == 9)
-                             <b style="color:red">
+                             <b style="color:red; font-size:13px;">
                         @endif
                         {{ $t->payment_status }}
                         </b>
@@ -65,7 +65,7 @@
                         @if($t->order_status_id == 6)
                         <div style="text-align:center"> 
                        
-                            <a onclick="return confirm('Are you sure you want to accept the order ID # {{ $t->order_id }} ?');" href="{{ route('paypal_validation_path', array($t->order_id, '8') ) }}" class="btn btn-success btn-xs"> Accept </a>
+                            <a onclick="return confirm('Are you sure you want to accept the order ID # {{ $t->order_id }} ?');" href="{{ route('paypal_validation_path', array($t->order_id, '8') ) }}" class="btn btn-success btn-xs" style="margin-bottom:5px;"> Accept </a>
                             <a onclick="return confirm('Are you sure you want to decline the order ID # {{ $t->order_id }} ?');"" href="{{ route('paypal_validation_path', array($t->order_id, '9')) }}" class="btn btn-danger btn-xs"> Decline </a>
                         </div>
                         @endif
